601c6291dae57ab0a721c3189de2646ebf050b39
Azello-XD <jonathanlenathea@gmail.com>
2025-05-05 18:30:43 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -11,7 +11,7 @@ API_ID = int(os.getenv("API_ID", "27418440"))
 
 API_HASH = os.getenv("API_HASH", "0a08a360e0e9f41b9896f655c300d09d")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7569019566:AAExDhuclLDukfKOZlKviNCdb2DOBM9B5A")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7307643391:AAHzXDu78-TDQIVw2OYh7dpamwnqNmGdoWw")
 
 OWNER_ID = int(os.getenv("OWNER_ID", "1927018403"))
 
@@ -19,6 +19,6 @@ BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002638098602").spl
 
 RMBG_API = os.getenv("RMBG_API", "VegrZSZEnnAsufcGsaxECDv6")
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://iyaaaaaa:anjingggg@iyaaanjing.uzkxmrz.mongodb.net/?retryWrites=true&w=majority&appName=iyaaanjing")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://YFarm:KontolXD#123@yfarm.emigrr1.mongodb.net/?retryWrites=true&w=majority&appName=YFarm")
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", " -1002638098602"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", " -1002647488482"))
